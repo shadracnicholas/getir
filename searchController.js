349b41a44ae35c4fa39b184aa124b84a6f8c1bb0
SHADRACK NICHOLAS <46599542+shadracnicholas@users.noreply.github.com>
2020-12-16 19:32:21 +0300

    Ensure its a valid date

--- searchController.js
+++ searchController.js
@@ -84,16 +84,17 @@ function validateRequest(requestPayload) {
     }
 };
 
+// Ensure its a valid date
 function isValidDate(dateString) {
     var regEx = /^\d{4}-\d{2}-\d{2}$/;
     if (!dateString.match(regEx)) {
         return false;
-    } // invalid format
+    }
     var d = new Date(dateString);
     var dNum = d.getTime();
     if (!dNum && dNum !== 0) {
         return false;
-    } // NaN value, invalid date
+    }
     return d.toISOString().slice(0, 10) === dateString;
 };
 
